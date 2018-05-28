ae8f71ca2dfcf0e1a66c94394de1495042ba2a43
zhangyu <zhangyu_mp@163.com>
2018-05-28 10:09:28 +0800

    练习

--- practiceDate/ViewController.swift
+++ practiceDate/ViewController.swift
@@ -16,15 +16,12 @@ class ViewController: UIViewController {
         let currentDate = Date()
         let dateFormatter = DateFormatter()
         DBLog(currentDate)
-        DBLog(currentDate.timeIntervalSince1970)
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         DBLog(dateFormatter.string(from: currentDate))
         let cal = Calendar.current
         
         let com = cal.dateComponents([Calendar.Component.era, Calendar.Component.year,Calendar.Component.month, Calendar.Component.day, Calendar.Component.hour, Calendar.Component.minute, Calendar.Component.second], from: currentDate)
- 
         DBLog(com)
-        
         let tyDate = dateFormatter.date(from: "\(com.year ?? 0)" + "-01-01 00:00:00") ?? Date()
         DBLog(tyDate)
         DBLog(tyDate.timeIntervalSince1970)
@@ -36,6 +33,63 @@ class ViewController: UIViewController {
         let currentYearLastDayInt = dft.date(from: "\(com.year ?? 0)-12-31")?.timeIntervalSince1970 ?? Double.greatestFiniteMagnitude  //当前年份最后一天timeint
         DBLog(currentYearFirstDayInt)
         DBLog(currentYearLastDayInt)
+        
+        var dateCom = DateComponents()
+        dateCom.day = 7
+        let addDate = cal.date(byAdding: dateCom, to: Date())
+        DBLog(addDate!)
+        
+        let dateComFom = DateComponentsFormatter()
+        dateComFom.unitsStyle = .full
+        let str = dateComFom.string(from: addDate!.timeIntervalSince(Date()))
+        DBLog(str!)
+        
+        //未来时间
+        let d1 = "2018-05-28 10:30:00"
+        let d11 = "2018-05-28 23:59:59"
+        
+        let d2 = "2018-05-29 00:00:00"
+        let d21 = "2018-05-29 10:00:00"
+        let d22 = "2018-05-29 23:59:59"
+        
+        
+        let d3 = "2018-05-30 00:00:00"
+        let d31 = "2018-05-30 10:00:00"
+        let d32 = "2018-05-30 23:59:59"
+        let d33 = "2018-12-31 23:59:59"
+        
+        let d4 = "2019-01-01 00:00:00"
+        let d41 = "2019-01-01 10:00:00"
+    
+        //过去时间
+        let d5 = "2018-05-28 10:05:00"
+        let d6 = "2018-05-28 09:59:00"
+        let d7 = "2018-05-27 11:00:00"
+        let d8 = "2018-05-22 11:00:00"
+        let d9 = "2018-05-20 11:00:00"
+        let d91 = "2018-01-01 00:00:00"
+        let d99 = "2017-12-31 23:59:59"
+        
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d1)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d11)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d2)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d21)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d22)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d3)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d31)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d32)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d33)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d4)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d41)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d5)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d6)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d7)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d8)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d9)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d91)!.timeIntervalSince1970))
+        DBLog(Date.customDateStr(dateSeconds: dateFormatter.date(from: d99)!.timeIntervalSince1970))
+        
+        
     }
     
    
@@ -179,7 +233,7 @@ extension Date {
         let afterTomorrowTimeInt = afterTomorrowDate?.timeIntervalSince1970 ?? Double.greatestFiniteMagnitude //后天0点timeint
         let currentYear = Calendar.current.dateComponents([Calendar.Component.year], from: Date()).year ?? 1970 //当前年份
         let currentYearFirstDayInt = dft.date(from: "\(currentYear)-01-01")?.timeIntervalSince1970 ?? Double.greatestFiniteMagnitude  //当前年份第一天timeint
-        let currentYearLastDayInt = dft.date(from: "\(currentYear)-12-31")?.timeIntervalSince1970 ?? Double.greatestFiniteMagnitude  //当前年份最后一天timeint
+        let currentYearLastDayInt = dft.date(from: "\(currentYear + 1)-01-01")?.timeIntervalSince1970 ?? Double.greatestFiniteMagnitude  //当前年份下一年开始时间timeint
         
         
         if timeDif > 0 { //未来时间
@@ -189,7 +243,7 @@ extension Date {
             else if dateSeconds >= tomorrowTimeInt && dateSeconds < afterTomorrowTimeInt { //大于明天0点小于后天0点
                 resultStr = "明天" + Date.HHmm(from: Date.init(timeIntervalSince1970: dateSeconds))
             }
-            else if dateSeconds > currentYearLastDayInt{ //超过今年 显示年份
+            else if dateSeconds >= currentYearLastDayInt{ //超过今年 显示年份
                 resultStr = Date.yyddhhmmss(from: Date.init(timeIntervalSince1970: dateSeconds))
             }
             else {
